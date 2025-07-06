41fcfbd4c972cac3a80f4103b5a19d238dc6d5e9
tapejaralira <tapejaralira@gmail.com>
2025-07-06 18:54:52 -0400

    adiciona estrutura inicial das classes e componentes do projeto de livros

---
+++ src/app/livro-lista/livro-lista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-lista',
+  imports: [],
+  templateUrl: './livro-lista.html',
+  styleUrl: './livro-lista.css'
+})
+export class LivroLista {
+
+}
